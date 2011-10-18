ea13e59c9898eec3d77ff01c8a408da9afe04fc5
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2011-10-18 14:42:15 +0200

    Add the main py file

---
+++ app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+import sys
+import os
+import codecs
+from markdown import markdown
+from bottle import\
+        Bottle,\
+        run,\
+        route,\
+        template,\
+        static_file,\
+        error,\
+        debug
+
+# Uncomment to run in a WSGI server
+#os.chdir(os.path.dirname(__file__))
+
+application = Bottle()
+debug(True)
+
+@application.route('/static/:filename')
+def server_static(filename):
+    """ Serve Static files """
+    return static_file(filename, root='static/')
+
+@application.error(404)
+def mistake404(code):
+    """ 404 Error """
+    return template('templates/404.html')
+
+@application.route('/page/:name')
+def show_page(name='index'):
+    filename = "src/{0}.mkd".format(name)
+    file = codecs.open(filename, 'r', encoding='utf8')
+    text = markdown(file.read())
+    template = open("templates/page.html",'r')
+    return str(template.read()).format(text)
+
+def main():
+    run(application, host='localhost', port=8080)
+    return 0
+
+if __name__ == '__main__': main()
+
